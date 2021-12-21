5f923ed0707bb2951657c50d20f9cf4bef202d8e
Bruno A. T. de Freitas <brunoatf257@gmail.com>
2021-12-21 02:37:13 -0300

    versao atualizada

--- logisticregression.py
+++ logisticregression.py
@@ -6,7 +6,7 @@ from celluloid import Camera
 class LogisticRegression:
 
     def sigmoid(self, inputs):
-        return 1 / (1 + exp(-np.dot(inputs, self.coefficients) + self.bias))
+        return 1 / (1 + exp(-np.dot(inputs, self.coefficients) - self.bias))
 
     def normalize(self, inputs):
         return inputs - self.train_mean
@@ -14,7 +14,7 @@ class LogisticRegression:
     def predict(self, inputs):
         return 1 if self.sigmoid(inputs) >= 0.5 else 0
 
-    def train(self, inputs, outputs, learning_rate, epochs, *generate_2d_animation):
+    def train(self, inputs, outputs, learning_rate, epochs, *generate_animation):
 
         '''Trains the model using a matrix of inputs - in which each line contains the data for one
         train case - and a vector of outputs. If the inputs have dimension 1 or 2, it is possible
@@ -25,7 +25,7 @@ class LogisticRegression:
         self.coefficients = np.zeros(inputs.shape[1]) #the values which multiply the input variables 
         self.bias = 0 #the bias is the linear coefficient of the linear equation used in the sigmoid's exponents
 
-        if generate_2d_animation:
+        if generate_animation:
             fig, ax = plt.subplots(1,2, figsize=[12,6])
             fig.suptitle("Logistic Regression training process:")
             ax[0].set_title("Mean squared error:")
@@ -37,7 +37,7 @@ class LogisticRegression:
 
             predicted_outputs = np.array([self.sigmoid(normalized_input) for normalized_input in normalized_inputs])
 
-            if generate_2d_animation:
+            if generate_animation:
 
                 mean_squared_errors.append((sum(outputs-predicted_outputs)**2)/inputs.shape[0])
                 ax[0].plot([i for i in range(1, len(mean_squared_errors)+1)], mean_squared_errors, color='black')
@@ -50,22 +50,20 @@ class LogisticRegression:
                 ax[1].set_ylabel("Probability of belonging to the red class")
                 camera.snap()
 
-            dloss_dexpoent = -2 * sum((outputs - predicted_outputs) * predicted_outputs * (1 - predicted_outputs))
-            self.bias -= learning_rate * dloss_dexpoent * (-1)
-
-            for index in range(self.coefficients.size): #we update each coefficient:
-
-                dloss_dcoefficient = dloss_dexpoent * normalized_inputs[index] * (-1)
-                self.coefficients[index] -= learning_rate * dloss_dcoefficient
+            self.bias -= learning_rate * -2 * sum((outputs - predicted_outputs) * predicted_outputs * (1 - predicted_outputs))
+            dloss_dcoefficients = np.zeros(self.coefficients.size)
+            for index in range(self.coefficients.size):
+                dloss_dcoefficients[index] += -2 * sum((outputs - predicted_outputs) * predicted_outputs * (1 - predicted_outputs) * normalized_inputs[:,index])
+            self.coefficients -= learning_rate * dloss_dcoefficients
         
-        if generate_2d_animation:
+        if generate_animation:
             anim = camera.animate()
             anim.save("animation_2d_logistic_regression.gif", fps=60)
         
     def test(self, inputs, expected_outputs):
         
         inputs = self.normalize(inputs)
-        outputs = [self.predict(input) for input in inputs]
+        outputs = np.array([self.predict(input) for input in inputs])
 
         count = 0
         for i in range(inputs.shape[0]):
@@ -78,6 +76,6 @@ class LogisticRegression:
 model = LogisticRegression()
 
 model.train(np.array([[1],[2],[3],[4],[5],[6],[7]]), np.array([1,1,1,0,0,0,0]), 0.1, 300, True)
-#model.test(np.array([1,2,3,4,5,6,7,8]), np.array([1,1,1,0,0,0,0,0]))
+model.test(np.array([[1],[2],[3],[4],[5],[6],[7],[8]]), np.array([0,0,0,1,1,1,1,1]))
 
 #Nota: se os dados de treino estiverem perfeitamente balanceados, a derivada do loss será 0 e vai dar merda
